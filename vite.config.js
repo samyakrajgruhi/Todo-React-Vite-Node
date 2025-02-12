3836f1543178d816dc2ad5c84da3f6e6c1f32a39
Samyak Rajgruhi <samyakrajgruhi@gmail.com>
2025-02-12 14:13:43 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: true, // Allows external access (for phone preview)
-    watch: {
-      usePolling: true,
-      interval: 100, // Reduce polling interval
-    }
-  },
   plugins: [react()],
 })
